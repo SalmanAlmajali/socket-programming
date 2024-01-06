6de9093ea4d74f4d253f759c9884b38a8d2d9409
SalmanAlmajali <89902695+SalmanAlmajali@users.noreply.github.com>
2024-01-06 20:27:35 +0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -11,22 +11,27 @@ public class Server {
 
     public void startServer() {
         try {
+            // Menjalankan server dan menerima koneksi dari klien
             while (!serverSocket.isClosed()) {
                 Socket socket = serverSocket.accept();
                 System.out.println("A new Client has Connected!");
+
+                // Membuat objek ClientHandler untuk menangani koneksi dengan klien
                 ClientHandler clientHandler = new ClientHandler(socket);
 
+                // Membuat dan menjalankan thread untuk menangani koneksi dengan klien secara paralel
                 Thread thread = new Thread(clientHandler);
                 thread.start();
             }
         } catch (IOException e) {
-
+             // Menangani IOException yang dapat terjadi saat menerima koneksi atau membuat thread
         }
     }
 
     public void closeServerSocket() {
         try {
             if (serverSocket != null) {
+                // Menutup serverSocket jika tidak null
                 serverSocket.close();
             }
         } catch (IOException e) {
@@ -35,8 +40,11 @@ public class Server {
     }
 
     public static void main(String[] args) throws IOException {
+        // Membuat serverSocket dan objek Server
         ServerSocket serverSocket = new ServerSocket(8000);
         Server server = new Server(serverSocket);
+
+        // Memulai server untuk menerima koneksi dari klien
         server.startServer();
     }
 }
